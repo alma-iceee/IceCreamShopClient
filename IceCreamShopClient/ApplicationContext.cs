01fcd9b234daae6885236352d802c4f769ea6d47
Sharipkhan_A <Sharipkhan_A@SHARIPKHAN_A>
2021-11-01 18:07:02 +0600

    Add project files.

---
+++ IceCreamShopClient/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Default;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace IceCreamShopClient
+{
+    public class ApplicationContext : Container
+    {
+        public ApplicationContext(IConfiguration configuration) : base(new Uri(configuration["URL"])) { }
+    }
+}
---
+++ IceCreamShopClient/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Default;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace IceCreamShopClient.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public ProductsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            
+            var goods = _context.Goods.Where(g => g.GoodsStatusId == 2).ToList();
+
+            var productIds = goods.GroupBy(g => g.ProductId).Select(g => new { ProductId = g.Key, Count = g.Count() });
+
+            var products = _context.Products.Where(p => productIds.Select(pI => pI.ProductId).Contains(p.Id)).ToList();
+
+            return View(goods);
+        }
+    }
+}
